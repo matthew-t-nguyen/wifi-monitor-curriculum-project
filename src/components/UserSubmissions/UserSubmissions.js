df52d6efd0811e00fcd8b6339653e37135a24a54
Matthew Nguyen <matthew.t.nguyen@berkeley.edu>
2024-03-14 00:59:11 -0700

    Connect frontend to backend

--- src/components/UserSubmissions/UserSubmissions.js
+++ src/components/UserSubmissions/UserSubmissions.js
@@ -1,59 +1,60 @@
 import { Button } from '@mui/material';
-import DropdownMenuLocation from '../DropdownMenu/DropdownMenuLocation.js';
-import DropdownMenuWifi from '../DropdownMenu/DropdownMenuWifi.js';
 import './UserSubmissions.css';
 import * as React from 'react';
-import { FormControl, useFormControlContext } from '@mui/base/FormControl';
+import { FormControl } from '@mui/base/FormControl';
 import { InputLabel, MenuItem, Select } from '@mui/material';
 
 function UserSubmissions() {
-    const [data, setData] = React.useState([]);
+    const [buttonClickCounter, setButtonClickCounter] = React.useState(0);
+
     const [scoreMLK, setScoreMLK] = React.useState(0);
     const [scoreGlade, setScoreGlade] = React.useState(0);
     const [scoreMoffit, setScoreMoffit] = React.useState(0);
-
-
-    const [location, setLocation] = React.useState('');
+    
+    const [loc, setLocation] = React.useState('');
+    const [wifi, setWifi] = React.useState(0);
   
     const handleLocation = (event) => {
       setLocation(event.target.value);
     };
-
-    const [wifi, setWifi] = React.useState('');
   
     const handleWifi = (event) => {
       setWifi(event.target.value);
     };
     
     function handleSubmit() {
-        fetch("http://localhost:3000/add-user", {   
+        fetch("http://localhost:2024/add-user", {   
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                loc: {location},
-                score: {wifi}
+                location: loc,
+                rating: wifi
             })
         });
 
-        fetch("http://localhost:3000/get-users", {   
+        setButtonClickCounter(buttonClickCounter + 1);
+    };
+
+    React.useEffect(() => {
+        fetch("http://localhost:2024/get-users", {   
             method: "GET",
         }).then(response => {
             return response.json();
         }).then(data => {
-            setData(data);
             let MLKScoreSum = 0;
             let MLKCounter = 0;
             let GladeScoreSum = 0;
             let GladeCounter = 0;
             let MoffitScoreSum = 0;
             let MoffitCounter = 0;
-            data.forEach(item => {
-                if (item.location == "MLK") {
+
+            data.results.forEach(item => {
+                if (item.location === "MLK") {
                     MLKScoreSum += item.rating;
                     MLKCounter += 1;
-                } else if (item.location == "Glade") {
+                } else if (item.location === "Glade") {
                     GladeScoreSum += item.rating;
                     GladeCounter += 1;
                 } else {
@@ -65,8 +66,9 @@ function UserSubmissions() {
             setScoreMLK(MLKScoreSum / MLKCounter);
             setScoreGlade(GladeScoreSum / GladeCounter);
             setScoreMoffit(MoffitScoreSum / MoffitCounter);
-        });
-    };
+
+        })}, [buttonClickCounter]);
+    
 
     return (
         <>
@@ -74,7 +76,7 @@ function UserSubmissions() {
                 <InputLabel id="simple-select-label">Where are you located?</InputLabel>
                 <Select
                     labelId="simple-select-label"
-                    value={location}
+                    value={loc}
                     onChange={handleLocation}
                 >
                     <MenuItem value={"MLK"}>MLK</MenuItem>
